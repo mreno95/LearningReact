0b86d83c416acc9fdbd1552b953e56fa7eaba8fc
Morgan Reno <mreno@g.clemson.edu>
2017-11-27 20:18:22 -0500

    learned handling events

--- src/app/index.js
+++ src/app/index.js
@@ -16,29 +16,50 @@ var TodoComponent = createReactClass({
     var todos = this.state.todos;
     todos = todos.map(function(item, index){
       return(
-        <TodoItem item={item} key={index} /> //nested component, passing it the items from the local version of todos array. Because we are outputting this component for each item in the array we need to give each component a key so react can have an identifier on that component
+        <TodoItem item={item} key={index} onDelete={this.onDelete} /> //nested component, passing it the items from the local version of todos array. Because we are outputting this component for each item in the array we need to give each component a key so react can have an identifier on that component
+        //passed onDelete function through an attribute so we can access it in the component below
       );
-    });
+    }.bind(this));//don't delete!!
       // inside return is where JSX is in action. everything in the return method needs to be contained in one parent tag
+      //on click is set to the function that will be fired when clicked
     return(
       <div id="todo-list">
         <p>Its the most wonderful time of the year...</p>
         <ul>{todos}</ul>
       </div>
     );
-  } //render
+  }, //render
+
+  // Custom Functions
+  //onDelete function is so that we can delete the array item and then pass this as a props to the component below?
+  onDelete: function(item){
+    var updatedTodos = this.state.todos.filter(function(val, index){
+      return item !== val;
+    }); //filtering item passed to us out of the array val = the string in the array and the updated array is saved in updatedTodos
+    this.setState({
+      todos: updatedTodos
+    });
+  }
 });
 
 //Create TodoItem component
+// If we want to be able to click on a list item and it will delete it, we need to create that event(function) in the component below because that is where the individual items are
 var TodoItem = createReactClass({
   render: function(){
     return (
       <li>
         <div className="todo-item">
           <span className="item-name">{this.props.item}</span>
+          <span className="item-delete" onClick={this.handleDelete}> x </span>
         </div>
       </li>
     );
+  },
+
+  //Custom Functions
+  //when on click function is fired we want to delete data from todos array
+  handleDelete: function(){
+    this.props.onDelete(this.props.item);
   }
 });
 
